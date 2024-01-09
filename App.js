663413dd653bea10c462d9ad89f773ffc2b86e62
Bhuvanagajjarapu <149143688+Bhuvanagajjarapu@users.noreply.github.com>
2024-01-09 12:30:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Cc from './Cc';
+export default class extends Component {
+  render() {
+    return (
+      <div>
+       <Cc />
+      </div>
+    )
+  }
+}
